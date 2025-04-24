191d16238d688f2599c39486cc74f0f36711bfff
PassusOh <87425805+ody0327@users.noreply.github.com>
2025-04-24 13:44:27 +0000

    8th day

--- main.js
+++ main.js
@@ -5,36 +5,68 @@ var url = require('url');
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if (_url == '/') {
-        title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';  
+            var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+        response.writeHead(200, { 'Content-Type': 'text/html' }); // Content-Type 추가
+        response.end(template);
+        });
+        } else {
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var title = queryData.id;
+            var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+        response.writeHead(200, { 'Content-Type': 'text/html' }); // Content-Type 추가
+        response.end(template);
+        });
+        }
+
+    } else {
+      response.writeHead(404, { 'Content-Type': 'text/html' }); // Content-Type 추가  
+      response.end('Not Found');
     }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200, { 'Content-Type': 'text/html' }); // Content-Type 추가
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-        var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-    `;
-    response.end(template);
-    })
+
+    
     
 });
 app.listen(3000);
