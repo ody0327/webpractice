53bbbb4104839329f3f778c166f613f2c6f42b86
PassusOh <87425805+ody0327@users.noreply.github.com>
2025-04-26 13:41:27 +0000

    10th day

---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.4)); // 반올림 함수
+console.log(Math.round(1.5));
+
+function sum(first, second) { // parameter  
+    return first + second;
+}
+
+
+console.log(sum(2, 4)); // arguments
